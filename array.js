5eb22dcb61ff6c83d80713b791a59280730a76d8
Fahadkhanreal <fhadikhan00@gmail.com>
2024-04-25 00:56:42 +0500

    push my todo-list to github

---
+++ array.js
@@ -0,0 +1,9 @@
+let fruitchat = ['banana', 'apple', 'mango'];
+//push()bad ma agar array ma kuch add krna ha to puah()use karaga.
+fruitchat.push('watermelon', 'kiwi');
+//pop() bad ma agar array ma sa kuch nikalna ho to pop() use karaga.yeh bhe array ke type ha.
+fruitchat.pop();
+//concat ([]) yeh array ka ander ek new list add krta hai.
+fruitchat = fruitchat.concat(['grapes', 'strawberry']);
+console.log(fruitchat);
+export {};
---
+++ arrays-and-loops.js
@@ -0,0 +1,22 @@
+let fruitchat = ['banana', 'apple', 'mango'];
+//push()bad ma agar array ma kuch add krna ha to puah()use karaga.
+fruitchat.push('watermelon', 'kiwi');
+//pop() bad ma agar array ma sa kuch nikalna ho to pop() use karaga.yeh bhe array ke type ha.
+fruitchat.pop();
+//concat ([]) yeh array ka ander ek new list add krta hai.
+fruitchat = fruitchat.concat(['grapes', 'strawberry']);
+console.log(fruitchat);
+//-----------------------------------------------
+//while loop yeh while loopsha
+let i = 0; //initalize
+while (i <= 10) {
+    console.log(i); //code execute
+    i++;
+}
+let ramdanDays = 0;
+while (ramdanDays <= 30) {
+    console.log("fasting");
+    console.log("fivetimesprayers");
+    ramdanDays++;
+}
+export {};
